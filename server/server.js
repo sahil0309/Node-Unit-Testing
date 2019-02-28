c4d560bbafc76ef7d9e89349544d6c70c7884d6b
Sahil Arora <sahila@kpit.com>
2019-02-28 16:41:29 +0530

    Encryption/Decryption added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express =  require('express');
+const cryptr = require('cryptr');
 const db = require('../db/db');
 
+const mycryptr = new cryptr('password');
 var app=express();
 
 app.get('/',(req,res)=>{
@@ -9,7 +11,13 @@ app.get('/',(req,res)=>{
     .catch((error)=>res.status(400).send(error));
     })
 
+app.post('/savePassword',(req,res)=>{
+    mycryptr.encrypt(req.body.password);
+})
 
+app.post('/login',(req,res)=>{
+    mycryptr.decrypt(req.body.password);
+})
 app.listen(3000,()=>console.log('Server Running on port 3000'));
 
 module.exports.app=app;
